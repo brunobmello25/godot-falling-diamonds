005574ce86470ffd3ff5a27361536d8371d7e961
Bruno Mello <bruno.barros.mello@gmail.com>
2024-05-19 01:06:17 -0300

    add player with movement + saw basic scene

---
+++ scenes/player/player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+
+var speed: float = 400
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta) -> void:
+	move(delta)
+
+
+func move(delta: float) -> void:
+	if Input.is_action_pressed("move_right"):
+		velocity.x = speed
+		sprite.set_flip_h(false)
+		sprite.play("walking")
+	elif Input.is_action_pressed("move_left"):
+		velocity.x = -speed
+		sprite.play("walking")
+		sprite.set_flip_h(true)
+	else:
+		velocity.x = 0
+		sprite.play("idle")
+
+	move_and_slide()
